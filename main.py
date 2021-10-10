1b85db9638d55317e37bf340035c1c850e66f76f
이종헌 <tlqkfwjrekdgl@gmail.com>
2021-10-10 15:46:21 +0900

    test

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import re
+if __name__ == '__main__':
+
+    f = open(sys.argv[1],'r')
+    data = f.readline().split(',')
+    print(data[0]+" +"+data[1])
+
+
+
+
+
+
+
+
